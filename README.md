ca33f49c950d0dfc8a89968c37d57ba2081a5b75
Thomas Yuill <thomas.yuill@gmail.com>
2014-05-05 18:58:32 -0400

    update
    Signed-off-by: Thomas Yuill <thomas.yuill@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-![alt tag](https://raw.github.com/danielauener/generator-wp-grunted-theme/master/header.png)
-
 # generator-wp-theme
 
 A WordPress theme [Yeoman](http://yeoman.io) generator, to kickstart WordPress
--- app/index.js
+++ app/index.js
@@ -8,10 +8,10 @@ var WpThemeGenerator = module.exports = function WpThemeGenerator(args, options,
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-  	if (this.themeNameSpace) {
-	  	process.chdir(this.themeNameSpace+"/grunt/");
-	    this.installDependencies({ skipInstall: options['skip-install'], bower: true });
-  	}
+    if (this.themeNameSpace) {
+      process.chdir(this.themeNameSpace+"/grunt/");
+      this.installDependencies({ skipInstall: options['skip-install'], bower: true });
+    }
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -25,61 +25,64 @@ WpThemeGenerator.prototype.askFor = function askFor() {
   console.log("\n\n                           "+chalk.blue.bold("**")+"         \n"+"           "+chalk.blue.bold("****")+"    "+chalk.blue.bold("****")+"    "+chalk.blue.bold("****")+"       \n"+"  "+chalk.yellow("GGGGG")+"  "+chalk.yellow("RRRR")+""+chalk.blue.bold("*")+"  "+chalk.yellow("U")+"   "+chalk.blue.bold("*")+""+chalk.yellow("U")+"  "+chalk.yellow("N")+"   "+chalk.blue.bold("*")+""+chalk.yellow("N")+"  "+chalk.yellow("TTTTTT")+"\n"+""+chalk.yellow("GG")+"       "+chalk.yellow("R")+"   "+chalk.yellow("R")+""+chalk.blue.bold("*")+" "+chalk.yellow("U")+"   "+chalk.blue.bold("*")+""+chalk.yellow("U")+""+chalk.blue.bold("*")+" "+chalk.yellow("N")+" "+chalk.yellow("N")+" "+chalk.blue.bold("*")+""+chalk.yellow("N")+"    "+chalk.yellow("TT")+"  \n"+""+chalk.yellow("GG")+"  "+chalk.yellow("GGG")+"  "+chalk.yellow("RRR")+"  "+chalk.blue.bold("**")+""+chalk.yellow("U")+"  "+chalk.blue.bold("*")+" "+chalk.yellow("U")+""+chalk.blue.bold("**")+""+chalk.yellow("N")+"  "+chalk.yellow("N")+" "+chalk.yellow("N")+"    "+chalk.yellow("TT")+"  \n"+"  "+chalk.yellow("GGGGG")+"  "+chalk.yellow("R")+"   "+chalk.yellow("R")+" "+chalk.blue.bold("**")+" "+chalk.yellow("UU")+"   "+chalk.blue.bold("*")+""+chalk.yellow("N")+" "+chalk.blue.bold("*")+"  "+chalk.yellow("N")+"    "+chalk.yellow("TT")+"  \n"+"                "+chalk.blue.bold("**")+"      "+chalk.blue.bold("**")+"            \n\n "+chalk.yellow.bold("*")+""+chalk.blue.bold("START YOUR GRUNTED WORDPRESS THEME")+""+chalk.yellow.bold("*")+" \n\n");
 
   var prompts = [{
-    name: 'themeName',
+    name:    'themeName',
     message: 'Name of the theme you want to create?'
   },{
     name: 'themeNameSpace',
     message: 'Uniq name-space for the theme (alphanumeric)?',
     default: function( answers ) {
-		return answers.themeName.replace(/\W/g, '').toLowerCase();
-	}
+    return   answers.themeName.replace(/\W/g, '').toLowerCase();
+  }
   },{
-    name: 'themeAuthor',
+    name:    'themeAuthor',
     message: 'Name of the themes author?',
     default: function( answers ) {
-		return 'John Doe';
-	}
+    return   'John Doe';
+  }
   },{
-    name: 'themeAuthorURI',
+    name:    'themeAuthorURI',
     message: 'Website of the themes authors?',
     default: function( answers ) {
-		return 'http://www.'+answers.themeAuthor.replace(/\W/g, '').toLowerCase()+'.com';
-	}
+    return   'http://www.'+answers.themeAuthor.replace(/\W/g, '').toLowerCase()+'.com';
+  }
   },{
-    name: 'themeURI',
+    name:    'themeURI',
     message: 'Website of the theme?',
-	default: function( answers ) {
-		return answers.themeAuthorURI+'/'+answers.themeNameSpace;
-	}
+    default: function( answers ) {
+    return   answers.themeAuthorURI+'/'+answers.themeNameSpace;
+  }
   },{
-  	type: 'checkbox',
-    name: 'themeTags',
+    type:    'checkbox',
+    name:    'themeTags',
     message: 'Theme tags ( more available on wordpress.org )?',
     choices: ['dark','light','left-sidebar','right-sidebar','fixed-layout','fluid-layout','responsive-layout']
   },{
-    name: 'themeDescription',
+    name:    'themeDescription',
     message: 'Description of the theme?',
     default: function( answers ) {
-		return 'This is a description for the '+answers.themeName+' theme.';
-	}
+    return   'This is a description for the '+answers.themeName+' theme.';
+  }
   }];
 
   this.prompt(prompts, function (props) {
-    this.themeName = props.themeName;
-    this.themeNameSpace = props.themeNameSpace;
-    this.themeAuthor = props.themeAuthor;
-    this.themeAuthorURI = props.themeAuthorURI;
-    this.themeURI = props.themeURI;
-    this.themeTags = props.themeTags;
+
+    this.themeName        = props.themeName;
+    this.themeURI         = props.themeURI;
+    this.themeTags        = props.themeTags;
+    this.themeAuthor      = props.themeAuthor;
+    this.themeAuthorURI   = props.themeAuthorURI;
+    this.themeNameSpace   = props.themeNameSpace;
     this.themeDescription = props.themeDescription;
-    this.jshintTag = '<%= jshint.all %>';
+
+    this.jshintTag        = '<%= jshint.all %>';
 
     cb();
   }.bind(this));
 };
 
 WpThemeGenerator.prototype.app = function app() {
-  var currentDate = new Date()
+
+  var currentDate   = new Date()
   this.themeCreated = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getDate();
 
   this.directory('theme', this.themeNameSpace);
