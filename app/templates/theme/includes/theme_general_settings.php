0557586b02416efce7876f152da1dea90fb86961
Thomas Yuill <thomas.yuill@gmail.com>
2015-01-14 18:16:39 -0500

    theme updates to general settings, added linked in account
    Signed-off-by: Thomas Yuill <thomas.yuill@gmail.com>

--- app/templates/theme/includes/theme_general_settings.php
+++ app/templates/theme/includes/theme_general_settings.php
@@ -2,8 +2,13 @@
 
 class new_general_setting {
   function new_general_setting() {
+     add_filter('admin_init', array( &$this, 'register_linkedin'));
      add_filter('admin_init', array( &$this, 'register_facebook'));
      add_filter('admin_init', array( &$this, 'register_twitter'));
+  }
+  function register_linkedin() {
+    register_setting('general', 'linkedin_account', 'esc_attr');
+    add_settings_field('link_account', '<label for="linkedin_account">'.__('LinkedIn Account', 'linkedin_account' ).'</label>', array(&$this, 'fields_linkedin'), 'general');
   }
   function register_twitter() {
     register_setting('general', 'twitter_account', 'esc_attr');
@@ -12,6 +17,10 @@ class new_general_setting {
   function register_facebook() {
       register_setting('general', 'facebook_account', 'esc_attr');
       add_settings_field('fb_account', '<label for="facebook_account">'.__('Facebook Account', 'facebook_account' ).'</label>', array(&$this, 'fields_facebook'), 'general' );
+  }
+  function fields_linkedin() {
+      $value = get_option('linkedin_account', '');
+      echo '<input type="text" id="linkedin_account" name="linkedin_account" value="' . $value . '" />';
   }
   function fields_twitter() {
       $value = get_option('twitter_account', '');
